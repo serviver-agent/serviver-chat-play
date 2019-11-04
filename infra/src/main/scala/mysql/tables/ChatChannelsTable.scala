b0e95b11ec9c21e94ffbd1f6db4bbff2df8f8bfe
KazuyaMiayshita <miy1924@gmail.com>
2019-11-04 23:34:31 +0900

    fmt

--- infra/src/main/scala/mysql/tables/ChatChannelsTable.scala
+++ infra/src/main/scala/mysql/tables/ChatChannelsTable.scala
@@ -11,10 +11,11 @@ object ChatChannelsTable extends SkinnyCRUDMapperWithId[ChannelId, ChatChannelRe
   override lazy val defaultAlias        = createAlias("c")
   override lazy val primaryKeyFieldName = "channel_id"
 
-  override def extract(rs: WrappedResultSet, rn: ResultName[ChatChannelRecord]): ChatChannelRecord = autoConstruct(rs, rn)
-  override def idToRawValue(channelId: ChannelId)                                          = channelId.display
-  override def rawValueToId(value: Any)                                                    = ChannelId(UUID.fromString(value.toString))
-  override def useExternalIdGenerator                                                      = true
+  override def extract(rs: WrappedResultSet, rn: ResultName[ChatChannelRecord]): ChatChannelRecord =
+    autoConstruct(rs, rn)
+  override def idToRawValue(channelId: ChannelId) = channelId.display
+  override def rawValueToId(value: Any)           = ChannelId(UUID.fromString(value.toString))
+  override def useExternalIdGenerator             = true
 
   def columnsAndValues(record: ChatChannelRecord) = List(
     Symbol("channel_id")   -> record.channel_id,
